8255722a9010fe330d96f1a5fb23e09fc64a0c55
Mohd Arshil Azim <119030009+mohdarshil09@users.noreply.github.com>
2025-01-11 16:04:29 +0530

    Delete src directory

--- src/store.js
+++
@@ -1,7 +0,0 @@
-// store.js
-import { createStore } from 'redux';
-import rootReducer from './reducer'; // Adjust the path if necessary
-
-const store = createStore(rootReducer);
-
-export default store;
