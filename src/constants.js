5a4195ae3f3cffc437b8176269598e264c07be39
Mohd Arshil Azim <119030009+mohdarshil09@users.noreply.github.com>
2025-11-20 17:48:33 +0530

    Add files via upload

---
+++ src/constants.js
@@ -0,0 +1,3 @@
+export const ACTIVE_BAR_COLOR = '#f97316';
+export const COMPLETED_BAR_COLOR = 'rgb(0, 182, 0)';
+
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux'; 
+import store from './store'; 
+import App from './App'; 
+
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>  {/* Wrap your app with Provider */}
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
